6ff7e565c3d4904b47912df856fd637e4d0c7adf
Ed Schouten <ed.schouten@prodrive-technologies.com>
2018-05-20 20:37:47 +0200

    Simplify the run_pdflatex.sh script a tiny bit.
    We can use the -jobname flag to alter the output, so we don't need to
    rely on calling basename(1).

--- run_pdflatex.sh
+++ run_pdflatex.sh
@@ -8,7 +8,7 @@ ENTRY="$1"
 shift
 FILES="$*"
 
-WRKDIR="$(mktemp -d ${TMPDIR:-/tmp}/bazel.XXXXXXXX)"
+WRKDIR="$(mktemp -d "${TMPDIR:-/tmp}/bazel.XXXXXXXX")"
 trap "rm -rf \"${WRKDIR}\"" EXIT
 SRCDIR="${WRKDIR}/src"
 LOGFILE="${WRKDIR}/log"
@@ -16,7 +16,7 @@ LOGFILE="${WRKDIR}/log"
 mkdir "${SRCDIR}"
 for file in ${FILES}
 do
-    case $file in
+    case "${file}" in
     *.tar)
         tar -C "${SRCDIR}" -xf "${file}"
         ;;
@@ -28,10 +28,10 @@ done
 
 cd "${SRCDIR}"
 for i in 1 2; do
-    if ! SOURCE_DATE_EPOCH=0 pdflatex "${ENTRY}" > "${LOGFILE}" 2>&1; then
+    if ! SOURCE_DATE_EPOCH=0 pdflatex -jobname=bazel_latex_output "${ENTRY}" > "${LOGFILE}" 2>&1; then
         cat "${LOGFILE}"
         exit 1
     fi
 done
 
-cp "${SRCDIR}/$(basename "${ENTRY}" .tex).pdf" "${OUT}"
+cp "${SRCDIR}/bazel_latex_output.pdf" "${OUT}"
